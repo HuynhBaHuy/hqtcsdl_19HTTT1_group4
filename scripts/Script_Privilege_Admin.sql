b83a40c9c114ade825912ea29f392b9c08ed5f33
dlxkhang <khangduong0902@gmail.com>
2021-10-29 19:31:34 +0700

    Update Script_Privilege_Admin.sql

--- scripts/Script_Privilege_Admin.sql
+++ scripts/Script_Privilege_Admin.sql
@@ -1,28 +1,30 @@
-﻿
-
------System Admin will have the permission to alter login/user accounts and to lock/unlock login/user accounts-----
--- Create login account for System Admin
+﻿-----System Admin will have the permission to alter login/user accounts and to lock/unlock login/user accounts-----
+-- Create a login account for System Admin (Assume that there is only one system admin)
 USE master
 exec sp_addLogin 'login_sysadmin','login_sysadmin'
-USE master
 GRANT ALTER ANY LOGIN TO login_sysadmin
 GO
----- Create user account for System Admin in database
-CREATE USER user_sysadmin FOR LOGIN login_sysadmin
+---- Create a user account for System Admin in database, this account will have a db owner permission
 USE OnlineOrderingSystem
-GRANT ALTER ANY USER TO user_sysadmin
+CREATE USER user_sysadmin FOR LOGIN login_sysadmin
+exec sp_addrolemember 'db_owner','user_sysadmin'
 GO
 
------Database Admin will have the permission to alter role
----- Create role database admin
+-----Database Admin will have the permission as a db owner
+-- Create three login accounts for Database Admins (Assume that there are three database admin)
+USE master
+exec sp_addLogin 'login_dbadmin1','login_dbadmin1'
+exec sp_addLogin 'login_dbadmin2','login_dbadmin2'
+exec sp_addLogin 'login_dbadmin3','login_dbadmin3'
+GO
+-- Create three user accounts for Database Admins
 USE OnlineOrderingSystem
-exec sp_addrole 'database_admin'
----- Add the user account of System Admin as role database_admin
-exec sp_addrolemember 'database_admin','user_sysadmin'
-GRANT ALTER ON ROLE::khach_hang TO database_admin
-GRANT ALTER ON ROLE::doi_tac TO database_admin
-GRANT ALTER ON ROLE::tai_xe TO database_admin
-GRANT ALTER ON ROLE::nhan_vien TO database_admin
-
--- GRANT SELECT TO database_admin WITH GRANT OPTION; PHẦN NÀY LÀM SAU
+CREATE USER user_dbadmin1 FOR LOGIN login_dbadmin1
+CREATE USER user_dbadmin2 FOR LOGIN login_dbadmin2
+CREATE USER user_dbadmin3 FOR LOGIN login_dbadmin3
+-- Add three user accounts of Database Admins into role db_owner
+exec sp_addrolemember 'db_owner','user_dbadmin1'
+exec sp_addrolemember 'db_owner','user_dbadmin2'
+exec sp_addrolemember 'db_owner','user_dbadmin3'
+GO
 
