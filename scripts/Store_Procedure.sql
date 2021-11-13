b40a11277413e1930d63c19fe184b1c3248d5e37
dlxkhang <khangduong0902@gmail.com>
2021-11-13 15:32:48 +0700

    Create Store_Procedure.sql

---
+++ scripts/Store_Procedure.sql
@@ -0,0 +1,211 @@
+﻿--unrepeatable read, phantom
+
+--Cập nhật thông tin tài khoản
+--Thêm – xóa –sửa tài khoản admin, nhân viên
+--Khóa và kích hoạt tài khoản
+
+-- view all procedure in db
+SELECT * 
+FROM OnlineOrderingSystem.INFORMATION_SCHEMA.ROUTINES
+WHERE ROUTINE_TYPE = 'PROCEDURE'
+
+SELECT SYSTEM_USER
+
+---------UPDATE INFORMATION OF LOGIN/USER ACCOUNT
+-- Change login name
+USE master
+GO
+CREATE PROCEDURE sp_changeLoginName @oldName nvarchar(30), @newName nvarchar(30)
+AS
+	BEGIN TRAN CHANGELOGINNAME
+		-- Check if current user is system admin
+		DECLARE @currentUser AS NVARCHAR(100)
+		SET @currentUser = (SELECT SYSTEM_USER)
+		IF @currentUser != 'login_sysadmin'
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN CHANGELOGINNAME
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@oldName) = 0 OR LEN(@newName) = 0
+					BEGIN
+						ROLLBACK TRAN CHANGELOGINNAME
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Alter login name
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'ALTER LOGIN ' + QUOTENAME(@oldName) + ' WITH NAME = ' + QUOTENAME(@newName)
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN CHANGELOGINNAME
+			END
+GO
+EXEC sp_ms_marksystemobject 'sp_changeLoginName'
+GO
+
+-- Change login password
+USE master
+GO
+CREATE PROCEDURE sp_changeLoginPassword @loginName nvarchar(30), @newPassword nvarchar(30)
+AS
+	BEGIN TRAN CHANGELOGINPASSWORD
+		-- Check if current user is system admin
+		DECLARE @currentUser AS NVARCHAR(100)
+		SET @currentUser = (SELECT SYSTEM_USER)
+		IF @currentUser != 'login_sysadmin'
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN CHANGELOGINPASSWORD
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@loginName) = 0 OR LEN(@newPassword) = 0
+					BEGIN
+						ROLLBACK TRAN CHANGELOGINPASSWORD
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Alter login password
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'ALTER LOGIN ' + QUOTENAME(@loginName) + ' WITH PASSWORD = ' + QUOTENAME(@newPassword)
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN CHANGELOGINPASSWORD
+			END
+GO
+EXEC sp_ms_marksystemobject 'sp_changeLoginPassword'
+GO
+
+
+-- Change user name in database
+USE OnlineOrderingSystem
+GO
+CREATE PROCEDURE sp_changeUsername @oldUsername nvarchar(30), @newUsername nvarchar(30)
+AS
+	BEGIN TRAN CHANGEUSERNAME
+		-- Check if current user is system admin
+		DECLARE @currentUser AS NVARCHAR(100)
+		SET @currentUser = (SELECT SYSTEM_USER)
+		IF @currentUser != 'login_sysadmin'
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN CHANGEUSERNAME
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@oldUsername) = 0 OR LEN(@newUsername) = 0
+					BEGIN
+						ROLLBACK TRAN CHANGEUSERNAME
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Alter user name
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'ALTER USER ' + QUOTENAME(@oldUsername) + ' WITH NAME = ' + QUOTENAME(@newUsername)
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN CHANGEUSERNAME
+			END
+GO
+
+---------ADD/DELETE/UPDATE ACCOUNT OF ADMINS AND EMPLOYEES
+-- Add login account for admin/employee
+USE master
+GO
+CREATE PROCEDURE sp_addLoginAccount @loginName nvarchar(30), @loginPassword nvarchar(30)
+AS
+	BEGIN TRAN ADDLOGINACCOUNT
+		-- Check if current user is system admin
+		DECLARE @currentUser AS NVARCHAR(100)
+		SET @currentUser = (SELECT SYSTEM_USER)
+		IF @currentUser != 'login_sysadmin'
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN ADDLOGINACCOUNT
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@loginName) = 0 OR LEN(@loginPassword) = 0
+					BEGIN
+						ROLLBACK TRAN ADDLOGINACCOUNT
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Add login
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'CREATE LOGIN ' + @loginName + ' WITH PASSWORD = ' + quotename(@loginPassword,'''')
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN ADDLOGINACCOUNT
+			END
+GO
+EXEC sp_ms_marksystemobject 'sp_addLoginAccount'
+GO
+
+-- Add user account for admin
+USE master
+GO
+CREATE PROCEDURE sp_addUserForAdmin @userName nvarchar(30), @loginName nvarchar(30)
+AS
+	BEGIN TRAN ADDUSERFORADMIN
+		-- Check if current user is system admin
+		DECLARE @currentUser AS NVARCHAR(100)
+		SET @currentUser = (SELECT SYSTEM_USER)
+		IF @currentUser != 'login_sysadmin'
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN ADDLOGINFORADMIN
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@userName) = 0 OR LEN(@loginName) = 0
+					BEGIN
+						ROLLBACK TRAN ADDUSERFORADMIN
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Add user for admin
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'USE OnlineOrderingSystem CREATE USER ' + @userName + ' FOR LOGIN ' + @loginName
+				EXECUTE sp_executesql @SQLQuery
+
+				-- Add role for admin
+				SET @SQLQuery = 'USE OnlineOrderingSystem ALTER ROLE db_owner ADD MEMBER ' + @userName
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN ADDUSERFORADMIN
+			END
+GO
+
+-- Add user account for employee
+USE master
+GO
+CREATE PROCEDURE sp_addUserForEmployee @userName nvarchar(30), @loginName nvarchar(30)
+AS
+	BEGIN TRAN ADDUSERFOREMPLOYEE
+		-- Check if current user is system admin
+		DECLARE @currentUser AS NVARCHAR(100)
+		SET @currentUser = (SELECT SYSTEM_USER)
+		IF @currentUser != 'login_sysadmin'
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN ADDUSERFOREMPLOYEE
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@userName) = 0 OR LEN(@loginName) = 0
+					BEGIN
+						ROLLBACK TRAN ADDUSERFOREMPLOYEE
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Add user for employee
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'USE OnlineOrderingSystem CREATE USER ' + @userName + ' FOR LOGIN ' + @loginName
+				EXECUTE sp_executesql @SQLQuery
+
+				-- Add role for employee
+				SET @SQLQuery = 'USE OnlineOrderingSystem ALTER ROLE db_owner ADD MEMBER ' + @userName
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN ADDUSERFOREMPLOYEE
+			END
+GO
+
+drop procedure sp_addUserForEmployee
