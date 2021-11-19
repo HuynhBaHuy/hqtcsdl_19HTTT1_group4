4a4bd8343a1bef743c35e78994c971f97871e688
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-11-19 09:45:30 +0700

    Fix converson deadlock

--- scripts/DEAD LOCK/TEST CASE 1/T1_TestCase1_Error.sql
+++ scripts/DEAD LOCK/TEST CASE 1/T1_TestCase1_Error.sql
@@ -5,32 +5,32 @@ GO
 CREATE PROCEDURE sp_deadlock_tc1_T1 @madt varchar(20), @madh varchar(20)
 AS
 BEGIN TRAN 
-IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
-BEGIN
-	ROLLBACK TRAN
-END
-ELSE
-BEGIN
-SET TRAN ISOLATION LEVEL SERIALIZABLE
-IF EXISTS (SELECT* FROM DON_HANG WITH (Holdlock) WHERE MaDT = @madt AND MaDH = @madh)
-BEGIN
-	Waitfor Delay '0:0:10';
-	UPDATE DON_HANG 
-	SET TinhTrangDH = 'Da xuat hang khoi kho'
-	WHERE MaDH = @madh
-	COMMIT TRAN;
-END
-ELSE
-BEGIN
-	ROLLBACK TRAN
-END
-END
+	IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
+		BEGIN
+			ROLLBACK TRAN
+		END
+	ELSE
+		BEGIN
+			SET TRAN ISOLATION LEVEL SERIALIZABLE
+			IF EXISTS (SELECT* FROM DON_HANG WITH (Holdlock) WHERE MaDT = @madt AND MaDH = @madh)
+				BEGIN
+					Waitfor Delay '0:0:10';
+					UPDATE DON_HANG WITH (XLOCK)
+					SET TinhTrangDH = 'Da xuat hang khoi kho'
+					WHERE MaDH = @madh
+					COMMIT TRAN;
+				END
+			ELSE
+				BEGIN
+					ROLLBACK TRAN
+				END
+		END
 
 GO 
 USE OnlineOrderingSystem
 GRANT EXEC ON sp_deadlock_tc1_T1
 TO doi_tac
 
---GO 
---USE OnlineOrderingSystem
---EXEC sp_deadlock_tc1_T1 '001','007'
+GO 
+USE OnlineOrderingSystem
+EXEC sp_deadlock_tc1_T1 '001','007'
--- scripts/DEAD LOCK/TEST CASE 1/T2_TestCase1_Error.sql
+++ scripts/DEAD LOCK/TEST CASE 1/T2_TestCase1_Error.sql
@@ -6,25 +6,25 @@ GO
 CREATE PROCEDURE sp_deadlock_tc1_T2 @madh varchar(20)
 AS
 BEGIN TRAN 
-IF IS_ROLEMEMBER('tai_xe') = 0 AND IS_ROLEMEMBER('dbowner') = 0
-BEGIN
-	ROLLBACK TRAN
-END
-ELSE
-BEGIN
-SET TRAN ISOLATION LEVEL SERIALIZABLE
-SELECT TenDuong FROM DON_HANG WITH (Holdlock) WHERE MaDH = @madh
-UPDATE DON_HANG 
-SET TinhTrangDH = 'Tai xe nhan hang'
-WHERE MaDH = @madh
-COMMIT TRAN
-END
+	IF IS_ROLEMEMBER('tai_xe') = 0 AND IS_ROLEMEMBER('dbowner') = 0
+		BEGIN
+			ROLLBACK TRAN
+		END
+	ELSE
+		BEGIN
+			SET TRAN ISOLATION LEVEL SERIALIZABLE
+			SELECT TenDuong FROM DON_HANG WITH (Holdlock) WHERE MaDH = @madh
+			UPDATE DON_HANG WITH (XLOCK)
+			SET TinhTrangDH = 'Tai xe nhan hang'
+			WHERE MaDH = @madh
+			COMMIT TRAN
+		END
 
 GO 
 USE OnlineOrderingSystem
 GRANT EXEC ON sp_deadlock_tc1_T2
 TO tai_xe
 
---GO 
---USE OnlineOrderingSystem
---EXEC sp_deadlock_tc1_T2 '007'
+GO 
+USE OnlineOrderingSystem
+EXEC sp_deadlock_tc1_T2 '007'
