7427e9568a4ac0655a00cf95a43f7bcc4eb39d29
huynhthuthao <thaohuynh13091@gmail.com>
2021-11-22 21:23:22 +0700

    update

--- scripts/LOST UPDATE/TEST CASE 1/T2_LostUpdate_error.sql
+++ scripts/LOST UPDATE/TEST CASE 1/T2_LostUpdate_error.sql
@@ -1,37 +1,49 @@
---T1 nhan vien 1 cua doi tac cap nhat thoi gian hieu luc hop dong 
---T2 nhan vien 2 cua doi tac cap nhat thoi gian hieu luc hop dong
+--T1 nhan vien 1 cap nhat thoi gian hieu luc hop dong cua doi tac
+--T2 nhan vien 2 cap nhat thoi gian hieu luc hop dong cua doi tac
 -- T2
 go
 use OnlineOrderingSystem
 
 GO
-CREATE PROCEDURE sp_lostupdate_tc1_T2 @mahd nvarchar(20), @tg_hlhd date
+CREATE PROCEDURE sp_lostupdate_tc1_T2 @masothue varchar(20), @mahd nvarchar(20), @madt nvarchar(20), @tghlhd date, @pthh float
 AS
 BEGIN TRAN 
-	IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
+	IF IS_ROLEMEMBER('nhan_vien') = 0 AND IS_ROLEMEMBER('dbowner') = 0
 		BEGIN
 			ROLLBACK TRAN
 		END
 	ELSE
 		BEGIN
-			IF EXISTS (SELECT * FROM HOP_DONG WHERE MaHD = @mahd)
-				BEGIN
-					SELECT TG_HieuLucHD FROM HOP_DONG WHERE MaHD = @mahd
-					UPDATE HOP_DONG SET TG_HieuLucHD = @tg_hlhd
-					WHERE MaHD = @mahd
-					COMMIT TRAN;
-				END
-			ELSE
+			IF NOT EXISTS(SELECT h.MaDT FROM DOI_TAC d JOIN HOP_DONG h ON (d.MaDT = h.MaDT) WHERE d.MaSoThue = @masothue)
 				BEGIN
 					ROLLBACK TRAN
+					PRINT('TRANSACTION IS ROLLBACKED')
+				END 
+			ELSE
+				BEGIN
+					-- Check if the input effective time is valid
+					IF(@tghlhd < (SELECT h.TG_HieuLucHD FROM HOP_DONG h WHERE h.MaDT = @madt))
+						BEGIN
+							ROLLBACK TRAN
+							PRINT('TRANSACTION IS ROLLBACKED')
+						END
+					ELSE
+						BEGIN
+							DECLARE @doanhsoban float
+							SET @doanhsoban = (SELECT SUM(d.TongPhiSP) FROM DON_HANG d WHERE d.MaDT = @madt)	
+							SELECT TG_HieuLucHD, PhanTramHoaHong FROM HOP_DONG WHERE MaDT = @madt and MaHD = @mahd
+							UPDATE HOP_DONG
+							SET TG_HieuLucHD = @tghlhd, PhanTramHoaHong = (@pthh * @doanhsoban) / 100
+							where MaDT = @madt
+							COMMIT TRAN;
+						END
+					END
 				END
-		END
-
 GO 
 USE OnlineOrderingSystem
 GRANT EXEC ON sp_lostupdate_tc1_T2
-TO doi_tac
+TO nhan_vien
 
 GO 
 USE OnlineOrderingSystem
-EXEC sp_lostupdate_tc1_T2 N'085','2027-01-05'
+EXEC sp_lostupdate_tc1_T2 '559','433', '813', '2029-05-06', 30000
