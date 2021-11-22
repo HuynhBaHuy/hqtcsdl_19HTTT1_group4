5fab5320f5c888c95241f10b0f3b100a59e3390d
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-11-22 21:13:50 +0700

    Update T1_LostUpdate_error.sql

--- scripts/LOST UPDATE/TEST CASE 1/SinhVien_19127551/T1_LostUpdate_error.sql
+++ scripts/LOST UPDATE/TEST CASE 1/SinhVien_19127551/T1_LostUpdate_error.sql
@@ -1,30 +1,43 @@
---T1 nhan vien 1 cua doi tac cap nhat thoi gian hieu luc hop dong va pham tram hoa hong
---T2 nhan vien 2 cua doi tac cap nhat thoi gian hieu luc hop dong va phan tram hoa hong
+--T1 nhan vien cap nhat thoi gian hieu luc hop dong va pham tram hoa hong cua doi tac
+--T2 nhan vien cap nhat thoi gian hieu luc hop dong va phan tram hoa hong cua doi tac
 go
 use OnlineOrderingSystem
+
 GO
-CREATE PROCEDURE sp_lostupdate_tc1_T1 @mahd nvarchar(20), @tg_hlhd date
+CREATE PROCEDURE sp_lostupdate_tc1_T2 @masothue varchar(20), @mahd nvarchar(20), @madt nvarchar(20), @tghlhd date, @pthh float
 AS
 BEGIN TRAN 
-	IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
+	IF IS_ROLEMEMBER('nhan_vien') = 0 AND IS_ROLEMEMBER('dbowner') = 0
 		BEGIN
 			ROLLBACK TRAN
 		END
 	ELSE
 		BEGIN
-			IF EXISTS (SELECT * FROM HOP_DONG WHERE MaHD = @mahd)
-				BEGIN
-					SELECT TG_HieuLucHD FROM HOP_DONG WHERE MaHD = @mahd
-					Waitfor Delay '0:0:10'
-					UPDATE HOP_DONG SET TG_HieuLucHD = @tg_hlhd
-					WHERE MaHD = @mahd
-					COMMIT TRAN;
-				END
-			ELSE
+			IF NOT EXISTS(SELECT h.MaDT FROM DOI_TAC d JOIN HOP_DONG h ON (d.MaDT = h.MaDT) WHERE d.MaSoThue = @masothue)
 				BEGIN
 					ROLLBACK TRAN
+					PRINT('TRANSACTION IS ROLLBACKED')
+				END 
+			ELSE
+				BEGIN
+					-- Check if the input effective time is valid
+					IF(@tghlhd < (SELECT h.TG_HieuLucHD FROM HOP_DONG h WHERE h.MaDT = @madt))
+						BEGIN
+							ROLLBACK TRAN
+							PRINT('TRANSACTION IS ROLLBACKED')
+						END
+					ELSE
+						BEGIN
+							DECLARE @doanhsoban float
+							SET @doanhsoban = (SELECT SUM(d.TongPhiSP) FROM DON_HANG d WHERE d.MaDT = @madt)	
+							SELECT TG_HieuLucHD, PhanTramHoaHong FROM HOP_DONG WHERE MaDT = @madt and MaHD = @mahd
+							UPDATE HOP_DONG
+							SET TG_HieuLucHD = @tghlhd, PhanTramHoaHong = (@pthh * @doanhsoban) / 100
+							where MaDT = @madt
+							COMMIT TRAN;
+						END
+					END
 				END
-		END
 
 
 
