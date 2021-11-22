653d2cca32d9ef084c10f62ef34c77655d7c44da
dlxkhang <khangduong0902@gmail.com>
2021-11-22 11:13:27 +0700

    Update Procedure.sql

--- scripts/Procedure.sql
+++ scripts/Procedure.sql
@@ -593,17 +593,13 @@ BEGIN TRAN
 			COMMIT TRAN
 		END 
 	END
-
+GO
 -----------------STORED PROCEDURE FOR ADMIN--------------------
 
----- Create a user account for System Admin in master database
----- to execute server-level stored procedure
-GO
-USE master
-CREATE USER user_sysadmin FOR LOGIN login_sysadmin
-GRANT EXECUTE to user_sysadmin
-GO
+
 ---------UPDATE INFORMATION OF LOGIN/USER ACCOUNT
+USE master
+GO
 -- Change login name
 CREATE PROCEDURE sp_changeLoginName @oldName nvarchar(30), @newName nvarchar(30)
 AS
@@ -630,7 +626,9 @@ AS
 				EXECUTE sp_executesql @SQLQuery
 				COMMIT TRAN CHANGELOGINNAME
 			END
-
+GO
+EXEC sp_ms_marksystemobject 'sp_changeLoginName'
+GO
 
 -- Change login password
 CREATE PROCEDURE sp_changeLoginPassword @loginName nvarchar(30), @newPassword nvarchar(30)
@@ -654,23 +652,24 @@ AS
 					END
 				-- Alter login password
 				DECLARE @SQLQuery nvarchar(500)
-				SET @SQLQuery = 'ALTER LOGIN ' + QUOTENAME(@loginName) + ' WITH PASSWORD = ' + QUOTENAME(@newPassword)
+				SET @SQLQuery = 'ALTER LOGIN ' + QUOTENAME(@loginName) + ' WITH PASSWORD = ' + QUOTENAME(@newPassword, '''')
 				EXECUTE sp_executesql @SQLQuery
 				COMMIT TRAN CHANGELOGINPASSWORD
 			END
-
+GO
+EXEC sp_ms_marksystemobject 'sp_changeLoginPassword'
+GO
 
 
 -- Change user name in database
 USE OnlineOrderingSystem
 GO
+
 CREATE PROCEDURE sp_changeUsername @oldUsername nvarchar(30), @newUsername nvarchar(30)
 AS
 	BEGIN TRAN CHANGEUSERNAME
-		-- Check if current user is system admin
-		DECLARE @currentUser AS NVARCHAR(100)
-		SET @currentUser = (SELECT SYSTEM_USER)
-		IF @currentUser != 'login_sysadmin'
+		-- Check if current role is db_owner
+		IF IS_ROLEMEMBER('db_owner') = 0
 			BEGIN
 				PRINT('You do not have permission to do this. Transaction rollback...')
 				ROLLBACK TRAN CHANGEUSERNAME
@@ -691,7 +690,71 @@ AS
 			END
 GO
 
----------ADD/DELETE ACCOUNT OF ADMINS AND EMPLOYEES
+----------------DELETE LOGIN/USER ACCOUNT
+
+-- Delete login account
+USE master
+GO
+
+CREATE PROCEDURE sp_deleteLoginAccount @loginName nvarchar(30)
+AS
+	BEGIN TRAN DELETELOGINACCOUNT
+		-- Check if current user is system admin
+		DECLARE @currentUser AS NVARCHAR(100)
+		SET @currentUser = (SELECT SYSTEM_USER)
+		IF @currentUser != 'login_sysadmin'
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN DELETELOGINACCOUNT
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@loginName) = 0
+					BEGIN
+						ROLLBACK TRAN DELETELOGINACCOUNT
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Delete user account
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'DROP LOGIN ' + @loginName
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN DELETELOGINACCOUNT
+			END
+GO
+EXEC sp_ms_marksystemobject 'sp_deleteLoginAccount'
+GO
+
+-- Delete user account of database
+USE OnlineOrderingSystem
+GO
+
+CREATE PROCEDURE sp_deleteUserAccount @userName nvarchar(30)
+AS
+	BEGIN TRAN DELETEUSERACCOUNT
+		-- Check if current role is db_owner
+		IF IS_ROLEMEMBER('db_owner') = 0
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN DELETEUSERACCOUNT
+			END
+		ELSE
+			BEGIN
+				-- Check invalid parameters
+				IF LEN(@userName) = 0
+					BEGIN
+						ROLLBACK TRAN DELETEUSERACCOUNT
+						PRINT('Invalid parameter(s). Transaction rollback...')
+					END
+				-- Delete user account
+				DECLARE @SQLQuery nvarchar(500)
+				SET @SQLQuery = 'USE OnlineOrderingSystem DROP USER ' + @userName
+				EXECUTE sp_executesql @SQLQuery
+				COMMIT TRAN DELETEUSERACCOUNT
+			END
+GO
+
+---------ADD LOGIN/USER ACCOUNT OF ADMINS AND EMPLOYEES
 -- Add login account for admin/employee
 USE master
 GO
@@ -720,16 +783,19 @@ AS
 				EXECUTE sp_executesql @SQLQuery
 				COMMIT TRAN ADDLOGINACCOUNT
 			END
-
+GO
+EXEC sp_ms_marksystemobject 'sp_addLoginAccount'
+GO
 
 -- Add user account for admin
+USE OnlineOrderingSystem
+GO
+
 CREATE PROCEDURE sp_addUserForAdmin @userName nvarchar(30), @loginName nvarchar(30)
 AS
 	BEGIN TRAN ADDUSERFORADMIN
-		-- Check if current user is system admin
-		DECLARE @currentUser AS NVARCHAR(100)
-		SET @currentUser = (SELECT SYSTEM_USER)
-		IF @currentUser != 'login_sysadmin'
+		-- Check if current role is db_owner
+		IF IS_ROLEMEMBER('db_owner') = 0
 			BEGIN
 				PRINT('You do not have permission to do this. Transaction rollback...')
 				ROLLBACK TRAN ADDLOGINFORADMIN
@@ -758,10 +824,8 @@ GO
 CREATE PROCEDURE sp_addUserForEmployee @userName nvarchar(30), @loginName nvarchar(30)
 AS
 	BEGIN TRAN ADDUSERFOREMPLOYEE
-		-- Check if current user is system admin
-		DECLARE @currentUser AS NVARCHAR(100)
-		SET @currentUser = (SELECT SYSTEM_USER)
-		IF @currentUser != 'login_sysadmin'
+		-- Check if current role is db_owner
+		IF IS_ROLEMEMBER('db_owner') = 0
 			BEGIN
 				PRINT('You do not have permission to do this. Transaction rollback...')
 				ROLLBACK TRAN ADDUSERFOREMPLOYEE
@@ -786,35 +850,11 @@ AS
 			END
 GO
 
--- Delete user account of admin/employee
-CREATE PROCEDURE sp_deleteUserAccount @userName nvarchar(30)
-AS
-	BEGIN TRAN DELETEUSERACCOUNT
-		-- Check if current user is system admin
-		DECLARE @currentUser AS NVARCHAR(100)
-		SET @currentUser = (SELECT SYSTEM_USER)
-		IF @currentUser != 'login_sysadmin'
-			BEGIN
-				PRINT('You do not have permission to do this. Transaction rollback...')
-				ROLLBACK TRAN DELETEUSERACCOUNT
-			END
-		ELSE
-			BEGIN
-				-- Check invalid parameters
-				IF LEN(@userName) = 0
-					BEGIN
-						ROLLBACK TRAN DELETEUSERACCOUNT
-						PRINT('Invalid parameter(s). Transaction rollback...')
-					END
-				-- Delete user account
-				DECLARE @SQLQuery nvarchar(500)
-				SET @SQLQuery = 'DROP USER ' + @userName
-				EXECUTE sp_executesql @SQLQuery
-				COMMIT TRAN DELETEUSERACCOUNT
-			END
-GO
 
 -- Lock login account of admin/employee
+USE master
+GO
+
 CREATE PROCEDURE sp_lockLoginAccount @loginName nvarchar(30)
 AS
 	BEGIN TRAN LOCKLOGINACCOUNT
@@ -871,13 +911,14 @@ AS
 GO
 
 -- Lock user account of admin/employee
+USE OnlineOrderingSystem
+GO
+
 CREATE PROCEDURE sp_lockUserAccount @userName nvarchar(30)
 AS
 	BEGIN TRAN LOCKUSERACCOUNT
-		-- Check if current user is system admin
-		DECLARE @currentUser AS NVARCHAR(100)
-		SET @currentUser = (SELECT SYSTEM_USER)
-		IF @currentUser != 'login_sysadmin'
+		-- Check if current role is db_owner
+		IF IS_ROLEMEMBER('db_owner') = 0
 			BEGIN
 				PRINT('You do not have permission to do this. Transaction rollback...')
 				ROLLBACK TRAN LOCKUSERACCOUNT
@@ -902,10 +943,8 @@ GO
 CREATE PROCEDURE sp_unlockUserAccount @userName nvarchar(30)
 AS
 	BEGIN TRAN UNLOCKUSERACCOUNT
-		-- Check if current user is system admin
-		DECLARE @currentUser AS NVARCHAR(100)
-		SET @currentUser = (SELECT SYSTEM_USER)
-		IF @currentUser != 'login_sysadmin'
+		-- Check if current role is db_owner
+		IF IS_ROLEMEMBER('db_owner') = 0
 			BEGIN
 				PRINT('You do not have permission to do this. Transaction rollback...')
 				ROLLBACK TRAN UNLOCKUSERACCOUNT
@@ -926,6 +965,113 @@ AS
 			END
 GO
 
-SELECT * 
-FROM master.INFORMATION_SCHEMA.ROUTINES
-WHERE ROUTINE_TYPE = 'PROCEDURE'
+
+-- Insert partner
+CREATE TYPE branchList AS TABLE (
+	maCN varchar(20),
+	maDT varchar(20),
+	maKV varchar(20),
+	diaChiCuThe nvarchar(50)
+)
+GO
+
+CREATE PROCEDURE sp_insertPartner 
+@maDT varchar(20),
+@tenDT nvarchar(50),
+@nguoiDaiDien nvarchar(50),
+@maKV varchar(20),
+@soChiNhanh int,
+@soLuongDH int,
+@maLoai varchar(20),
+@diaChiKD nvarchar(50),
+@soDT varchar(15),
+@email varchar(50),
+@maSoThue varchar(20),
+@danhSachChiNhanh branchList READONLY
+AS
+	BEGIN TRAN INSERTPARTNER
+		-- Check if current user has db_owner role
+		IF IS_ROLEMEMBER('db_owner') = 0
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN INSERTPARTNER
+			END
+		ELSE
+			BEGIN
+				-- Check if partner exist
+				IF EXISTS (SELECT * FROM DOI_TAC WHERE MaDT = @maDT)
+					BEGIN
+						ROLLBACK TRAN INSERTPARTNER
+						PRINT('Partner already exists. Transaction rollback...')
+					END
+				-- Insert partner
+				INSERT INTO DOI_TAC VALUES(@maDT, @tenDT, @nguoiDaiDien, @maKV, @soChiNhanh, @soLuongDH, @maLoai, @diaChiKD, @soDT, @email, @maSoThue)
+				PRINT('AFTER INSERT DOI_TAC')
+				-- Insert list of branches
+				INSERT INTO CHI_NHANH SELECT * FROM @danhSachChiNhanh
+				PRINT('AFTER INSERT CHI_NHANH')
+				COMMIT TRAN INSERTPARTNER
+			END
+GO
+
+-- Insert customer
+CREATE PROCEDURE sp_insertCustomer
+@maKH varchar(20),
+@tenKH nvarchar(50),
+@soDT varchar(15)
+AS
+	BEGIN TRAN INSERTCUSTOMER
+		-- Check if current user has db_owner role
+		IF IS_ROLEMEMBER('db_owner') = 0
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN INSERTPARTNER
+			END
+		ELSE
+			BEGIN
+				-- Check if customer exist
+				IF EXISTS (SELECT * FROM KHACH_HANG WHERE MaKH = @maKH)
+					BEGIN
+						ROLLBACK TRAN INSERTCUSTOMER
+						PRINT('Customer already exists. Transaction rollback...')
+					END
+				-- Insert customer
+				INSERT INTO KHACH_HANG VALUES(@maKH, @tenKH, @soDT)
+				
+				COMMIT TRAN INSERTCUSTOMER
+			END
+GO
+
+-- Insert driver
+CREATE PROCEDURE sp_insertDriver
+@maTX varchar(20),
+@tenTX nvarchar(50),
+@cmnd varchar(15),
+@soDT varchar(15),
+@diaChi nvarchar(100),
+@bienSo varchar(15),
+@maKV varchar(20),
+@email varchar(50),
+@taiKhoanNH nvarchar(100)
+AS
+	BEGIN TRAN INSERTDRIVER
+		-- Check if current user has db_owner role
+		IF IS_ROLEMEMBER('db_owner') = 0
+			BEGIN
+				PRINT('You do not have permission to do this. Transaction rollback...')
+				ROLLBACK TRAN INSERTDRIVER
+			END
+		ELSE
+			BEGIN
+				-- Check if driver exist
+				IF EXISTS (SELECT * FROM TAI_XE WHERE MaTX = @maTX)
+					BEGIN
+						ROLLBACK TRAN INSERTDRIVER
+						PRINT('Customer already exists. Transaction rollback...')
+					END
+				-- Insert driver
+				INSERT INTO TAI_XE VALUES(@maTX, @tenTX, @cmnd, @soDT, @diaChi, @bienSo, @maKV, @email, @taiKhoanNH)
+				
+				COMMIT TRAN INSERTDRIVER
+			END
+GO
