0a8135a606a9fafd3190b9273cd061e35c03bef2
dlxkhang <khangduong0902@gmail.com>
2021-12-12 22:15:12 +0700

    add document about fixed phantom read

---
+++ scripts/PHANTOM/Test_case_1_19127432/T2_fixed.sql
@@ -0,0 +1,32 @@
+USE OnlineOrderingSystem
+GO
+
+CREATE PROCEDURE sp_DeleteContract_T2_fixed @madt varchar(20)
+AS
+BEGIN TRAN
+	IF IS_ROLEMEMBER('nhan_vien') = 0 AND IS_ROLEMEMBER('db_owner') = 0
+		BEGIN 
+			ROLLBACK TRAN
+			PRINT('TRANSACTION IS ROLLBACKED')
+		END
+	ELSE 
+		BEGIN
+			IF NOT EXISTS(SELECT h.MaHD FROM HOP_DONG h WITH(XLOCK) WHERE MaDT = @madt)
+				BEGIN
+					ROLLBACK TRAN
+					PRINT('TRANSACTION IS ROLLBACKED')
+				END 
+			ELSE
+				BEGIN
+					DECLARE @mahd varchar(20)
+					SET @mahd = (SELECT h.MaHD FROM HOP_DONG h WHERE MaDT = @madt)
+
+					DELETE FROM CT_HOPDONG
+					WHERE MaHD = @mahd
+
+					DELETE FROM HOP_DONG
+					WHERE MaHD = @mahd
+
+					COMMIT
+				END
+		END
