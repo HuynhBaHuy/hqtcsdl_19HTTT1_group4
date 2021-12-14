303f7483cbf67ae30a4faf65e85cb881fcce04d8
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-14 11:18:49 +0700

    Update T1_fixed.sql

--- scripts/LOST UPDATE/Test_case_1_19127551/T1_fixed.sql
+++ scripts/LOST UPDATE/Test_case_1_19127551/T1_fixed.sql
@@ -4,7 +4,7 @@ go
 use OnlineOrderingSystem
 
 GO
-CREATE PROCEDURE sp_lostupdate_tc1_T1 @masothue varchar(20), @mahd nvarchar(20), @madt nvarchar(20), @tghlhd date, @pthh float
+CREATE PROCEDURE sp_lostupdate_tc1_T1_fixed @masothue varchar(20), @mahd nvarchar(20), @madt nvarchar(20), @tghlhd date, @pthh float
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('nhan_vien') = 0 AND IS_ROLEMEMBER('dbowner') = 0
