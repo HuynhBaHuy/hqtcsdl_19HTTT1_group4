3e23687237a662c99bc4a5c1594d13f3a44b937e
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-12-14 17:14:01 +0700

    update script deadlock for testcase 1- T2,

--- scripts/DEAD LOCK/Test_case_1_19127420/T2_error.sql
+++ scripts/DEAD LOCK/Test_case_1_19127420/T2_error.sql
@@ -3,7 +3,7 @@
 --Test case 1
 -- T2 - Tai Xe
 GO
-CREATE PROCEDURE sp_deadlock_tc1_T2_error @madh varchar(20), @ttdh nvarchar(50)
+CREATE PROCEDURE sp_deadlock_tc1_T2_error @madh varchar(20), @ttdh nvarchar(50), @matx varchar(20)
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('tai_xe') = 0 AND IS_ROLEMEMBER('dbowner') = 0
@@ -12,9 +12,19 @@ BEGIN TRAN
 		END
 	ELSE
 		BEGIN
-			SELECT TenDuong FROM DON_HANG WITH (Holdlock) WHERE MaDH = @madh
-			UPDATE DON_HANG WITH (XLOCK)
-			SET TinhTrangDH = @ttdh
-			WHERE MaDH = @madh
-			COMMIT TRAN
+			IF EXISTS (SELECT* FROM GIAO_HANG WHERE MaDH = @madh AND MaTX = @matx)
+				BEGIN
+					ROLLBACK TRAN
+				END
+			ELSE
+				BEGIN
+					SELECT TenDuong FROM DON_HANG WITH (Holdlock) WHERE MaDH = @madh
+					UPDATE DON_HANG WITH (XLOCK)
+					SET TinhTrangDH = @ttdh
+					WHERE MaDH = @madh
+					
+					INSERT INTO GIAO_HANG
+					VALUES (@matx,@madh,NULL)
+					COMMIT TRAN				
+				END
 		END
