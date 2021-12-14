f5b39ddb70f68ddde68c265a0d00ff16e3ab0043
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-14 11:03:58 +0700

    Update T2_error.sql

--- scripts/DIRTY READ/Test_case_2_19127551/T2_error.sql
+++ scripts/DIRTY READ/Test_case_2_19127551/T2_error.sql
@@ -1,11 +1,10 @@
 --T1 nhan vien xem danh sach hop dong
 --T2 doi tac cap nhat hop dong 
--- DROP PROC sp_dirtyread_tc2_T2
--- DROP PROC sp_dirtyread_tc2_T1
+
 go
 use OnlineOrderingSystem
 GO
-CREATE PROCEDURE sp_dirtyread_tc2_T2 @mahd nvarchar(20), @madt nvarchar(20), @tghlhd date
+CREATE PROCEDURE sp_dirtyread_T2_error @mahd nvarchar(20), @madt nvarchar(20), @tghlhd date
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
@@ -23,11 +22,3 @@ BEGIN TRAN
 		-- Cancel update due to lost network. Rollback transaction
 		ROLLBACK TRANSACTION
 
-GO 
-USE OnlineOrderingSystem
-GRANT EXEC ON sp_dirtyread_tc2_T2
-TO doi_tac
-
---GO 
---USE OnlineOrderingSystem
---EXEC sp_dirtyread_tc2_T2 N'085', N'057', '2026-05-06'
