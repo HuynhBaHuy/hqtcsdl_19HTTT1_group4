e5bb62f07da4aef3722827f7cbd6734713057db5
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-14 11:24:18 +0700

    Update T2_fixed.sql

--- scripts/LOST UPDATE/Test_case_3_19127551/T2_fixed.sql
+++ scripts/LOST UPDATE/Test_case_3_19127551/T2_fixed.sql
@@ -4,7 +4,7 @@ go
 use OnlineOrderingSystem
 
 GO
-CREATE PROCEDURE sp_lostupdate_tc3_T2 @madh nvarchar(20), @ttdh nvarchar(50)
+CREATE PROCEDURE sp_lostupdate_tc3_T2_fixed @madh nvarchar(20), @ttdh nvarchar(50)
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('tai_xe') = 0 AND IS_ROLEMEMBER('dbowner') = 0
@@ -26,7 +26,3 @@ BEGIN TRAN
 					ROLLBACK TRAN
 				END
 		END
-
---drop proc sp_lostupdate_tc3_T2
---go
---exec sp_lostupdate_tc3_T2 '005', N'Dang gui hang'
