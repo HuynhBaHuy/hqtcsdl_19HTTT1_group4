bd87038b3f4df113c7a7bd8b4adaf822456b1e96
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-14 11:02:29 +0700

    Update T2_fixed.sql

--- scripts/DIRTY READ/Test_case_1_19127551/T2_fixed.sql
+++ scripts/DIRTY READ/Test_case_1_19127551/T2_fixed.sql
@@ -3,7 +3,7 @@
 go
 use OnlineOrderingSystem
 GO
-CREATE PROCEDURE sp_dirtyread_tc1_T2 @madh nvarchar(20)
+CREATE PROCEDURE sp_dirtyread_T2_fixed @madh nvarchar(20)
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('khach_hang') = 0 AND IS_ROLEMEMBER('dbowner') = 0
@@ -23,6 +23,3 @@ BEGIN TRAN
 					ROLLBACK TRAN
 				END
 		END
-go
---drop proc sp_dirtyread_tc1_T2 
-exec sp_dirtyread_tc1_T2 '005'
