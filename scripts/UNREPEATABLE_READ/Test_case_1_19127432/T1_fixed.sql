6b9469c463a35ff10c93f74dd7916b97113a4ef5
dlxkhang <khangduong0902@gmail.com>
2021-12-14 10:39:29 +0700

    update missing scripts

--- scripts/UNREPEATABLE_READ/Test_case_1_19127432/T1_fixed.sql
+++ scripts/UNREPEATABLE_READ/Test_case_1_19127432/T1_fixed.sql
@@ -1,7 +1,7 @@
 ﻿USE OnlineOrderingSystem
 GO
 
-CREATE PROCEDURE spUpdateOrderStatusForDriver_T1_error @matx varchar(20), @madh varchar(20), @ttdh nvarchar(50)
+CREATE PROCEDURE spUpdateOrderStatusForDriver_T1_fixed @matx varchar(20), @madh varchar(20), @ttdh nvarchar(50)
 AS
 BEGIN TRAN
 	IF IS_ROLEMEMBER('tai_xe') = 0 AND IS_ROLEMEMBER('db_owner') = 0
@@ -19,7 +19,7 @@ BEGIN TRAN
 			ELSE
 				BEGIN
 					Waitfor Delay '0:0:10'
-					IF (SELECT TinhTrangDH FROM DON_HANG WHERE MaDH = @madh) = 'Đã hoàn trả hàng'
+					IF (SELECT TinhTrangDH FROM DON_HANG WITH(XLOCK) WHERE MaDH = @madh) = 'Đã hoàn trả hàng'
 						-- Not allow to update order status when the order is refunded
 						BEGIN
 							ROLLBACK TRAN
