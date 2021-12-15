b3b078d77fdefdeac5763c48a92578e071688c15
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-12-15 22:29:07 +0700

    add T2_fixed for deadlock

---
+++ scripts/DEAD LOCK/Test_case_2_19127420/T2_fixed.sql
@@ -0,0 +1,30 @@
+--Cycle deadlock
+--Test case 2
+-- T2 - Doi Tac
+USE OnlineOrderingSystem
+GO
+CREATE PROCEDURE sp_deadlock_tc2_T2_fixed @madh varchar(20), @madt varchar(20), @ttdh nvarchar(50)
+AS
+BEGIN TRAN 
+	IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
+		BEGIN
+			ROLLBACK TRAN
+		END
+	ELSE
+		BEGIN
+			IF NOT EXISTs(SELECT MaDH FROM DON_HANG WHERE MaDH = @madh AND MaDT = @madt)
+			BEGIN
+				ROLLBACK TRAN
+			END
+			ELSE
+			BEGIN
+				UPDATE DON_HANG WITH (XLOCK)
+				SET TinhTrangDH = @ttdh
+				WHERE MaDH = @madh
+
+				SELECT MaTX  
+				FROM GIAO_HANG WITH (HOLDLOCK)
+				WHERE MaDH = @madh
+				COMMIT TRAN
+			END
+		END
