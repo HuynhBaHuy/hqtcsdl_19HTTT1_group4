068074c31ff4e7187923be2e87a3c4677f0f30a4
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-15 08:42:46 +0700

    Update T2_fixed.sql

--- scripts/DIRTY READ/Test_case_1_19127551/T2_fixed.sql
+++ scripts/DIRTY READ/Test_case_1_19127551/T2_fixed.sql
@@ -3,7 +3,7 @@
 go
 use OnlineOrderingSystem
 GO
-CREATE PROCEDURE sp_dirtyread_tc1_T2_fixed @madh nvarchar(20)
+CREATE PROCEDURE sp_dirtyread_tc1_T2_fixed @madh varchar(20)
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('khach_hang') = 0 AND IS_ROLEMEMBER('dbowner') = 0
