dc9d169d3f970c4b7355bde8edaf58bc71574c32
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-15 08:46:54 +0700

    Update T2_error.sql

--- scripts/DIRTY READ/Test_case_2_19127551/T2_error.sql
+++ scripts/DIRTY READ/Test_case_2_19127551/T2_error.sql
@@ -4,21 +4,38 @@
 go
 use OnlineOrderingSystem
 GO
-CREATE PROCEDURE sp_dirtyread_tc2_T2_error @mahd varchar(20), @madt varchar(20), @tghlhd date
+CREATE PROCEDURE sp_dirtyread_tc2_T2_error @masothue varchar(20), @mahd varchar(20), @madt varchar(20), @tghlhd date, @pthh float
 AS
 BEGIN TRAN 
-	IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
+	IF IS_ROLEMEMBER('nhan_vien') = 0 AND IS_ROLEMEMBER('dbowner') = 0
 		BEGIN
 			ROLLBACK TRAN
 		END
 	ELSE
-		IF EXISTS (SELECT * FROM HOP_DONG WHERE MaDT = @madt and MaHD = @mahd)
-			BEGIN
-				 SELECT TG_HieuLucHD FROM HOP_DONG WHERE MaDT = @madt and MaHD = @mahd    
-				 UPDATE HOP_DONG SET TG_HieuLucHD = @tghlhd WHERE MaDT = @madt and MaHD = @mahd
-				-- Waiting for updating
-				Waitfor Delay '00:00:10'
-			END
-		-- Cancel update due to lost network. Rollback transaction
-		ROLLBACK TRANSACTION
-
+			IF NOT EXISTS(SELECT h.MaDT FROM DOI_TAC d JOIN HOP_DONG h ON (d.MaDT = h.MaDT) WHERE d.MaSoThue = @masothue)
+				BEGIN
+					ROLLBACK TRAN
+					PRINT('TRANSACTION IS ROLLBACKED')
+				END 
+			ELSE
+				BEGIN
+					DECLARE @doanhsoban float
+					-- Check if the input effective time is valid
+					IF(@tghlhd < (SELECT h.TG_HieuLucHD FROM HOP_DONG h WHERE h.MaDT = @madt))
+						BEGIN
+							ROLLBACK TRAN
+							PRINT('TRANSACTION IS ROLLBACKED')
+						END
+					ELSE
+						BEGIN	
+							SELECT TG_HieuLucHD, PhanTramHoaHong FROM HOP_DONG WHERE MaDT = @madt and MaHD = @mahd
+							SET @doanhsoban = (SELECT SUM(d.TongPhiSP) FROM DON_HANG d WHERE d.MaDT = @madt)
+							UPDATE HOP_DONG
+							SET TG_HieuLucHD = @tghlhd, PhanTramHoaHong = (@pthh * @doanhsoban) / 100
+							where MaDT = @madt
+							-- Waiting for updating
+							Waitfor Delay '00:00:10'
+						END
+						-- Cancel update due to lost network. Rollback transaction
+						ROLLBACK TRANSACTION
+				END
