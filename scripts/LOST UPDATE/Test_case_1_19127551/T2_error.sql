d25a07166938a166e4ff606cc17fa865a958e358
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-15 13:11:32 +0700

    Update T2_error.sql

--- scripts/LOST UPDATE/Test_case_1_19127551/T2_error.sql
+++ scripts/LOST UPDATE/Test_case_1_19127551/T2_error.sql
@@ -14,7 +14,7 @@ BEGIN TRAN
 		END
 	ELSE
 		BEGIN
-			IF NOT EXISTS(SELECT h.MaDT FROM DOI_TAC d JOIN HOP_DONG h ON (d.MaDT = h.MaDT) WHERE d.MaSoThue = @masothue)
+			IF NOT EXISTS(SELECT h.TG_HieuLucHD FROM DOI_TAC d JOIN HOP_DONG h ON (d.MaDT = h.MaDT) WHERE d.MaSoThue = @masothue)
 				BEGIN
 					ROLLBACK TRAN
 					PRINT('TRANSACTION IS ROLLBACKED')
@@ -22,19 +22,18 @@ BEGIN TRAN
 			ELSE
 				BEGIN
 					-- Check if the input effective time is valid
-					IF(@tghlhd < (SELECT h.TG_HieuLucHD FROM HOP_DONG h WHERE h.MaDT = @madt))
+					Waitfor Delay '00:00:10'
+					IF(@tg_hlhd < (SELECT h.TG_HieuLucHD FROM DOI_TAC d JOIN HOP_DONG h ON (d.MaDT = h.MaDT) WHERE d.MaSoThue = @masothue))
 						BEGIN
 							ROLLBACK TRAN
 							PRINT('TRANSACTION IS ROLLBACKED')
 						END
 					ELSE
 						BEGIN
-							DECLARE @doanhsoban float
-							SET @doanhsoban = (SELECT SUM(d.TongPhiSP) FROM DON_HANG d WHERE d.MaDT = @madt)	
-							SELECT TG_HieuLucHD, PhanTramHoaHong FROM HOP_DONG WHERE MaDT = @madt and MaHD = @mahd
+							SELECT h.TG_HieuLucHD, h.PhanTramHoaHong FROM DOI_TAC d JOIN HOP_DONG h ON (d.MaDT = h.MaDT) WHERE d.MaSoThue = @masothue)
 							UPDATE HOP_DONG
-							SET TG_HieuLucHD = @tghlhd, PhanTramHoaHong = (@pthh * @doanhsoban) / 100
-							where MaDT = @madt
+							SET TG_HieuLucHD = @tg_hlhd, PhanTramHoaHong = @pthh
+							where MaDT IN (SELECT MaDT FROM DOI_TAC WHERE MaSoThue = @masothue)
 							COMMIT TRAN;
 						END
 					END
