61c1211979e377cbabc40d800b4a7200f84428c3
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-15 08:51:08 +0700

    Update T2_fixed.sql

--- scripts/LOST UPDATE/Test_case_2_19127551/T2_fixed.sql
+++ scripts/LOST UPDATE/Test_case_2_19127551/T2_fixed.sql
@@ -2,7 +2,7 @@
 --T2 nhan vien 2 cua doi tac cap nhat gia san pham
 use OnlineOrderingSystem
 GO
-CREATE PROCEDURE sp_lostupdate_tc2_T2_fixed @masp nvarchar(20), @giasp float
+CREATE PROCEDURE sp_lostupdate_tc2_T2_fixed @masp varchar(20), @giasp float
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('doi_tac') = 0 AND IS_ROLEMEMBER('dbowner') = 0
