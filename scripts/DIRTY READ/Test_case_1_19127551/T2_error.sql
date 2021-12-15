8442e82351252d9276fbc14a2ebc8cb2b252a43b
Huỳnh Thu Thảo <63647679+hthuthao@users.noreply.github.com>
2021-12-15 08:42:32 +0700

    Update T2_error.sql

--- scripts/DIRTY READ/Test_case_1_19127551/T2_error.sql
+++ scripts/DIRTY READ/Test_case_1_19127551/T2_error.sql
@@ -3,7 +3,7 @@
 go
 use OnlineOrderingSystem
 GO
-CREATE PROCEDURE sp_dirtyread_tc1_T2_error @madh nvarchar(20)
+CREATE PROCEDURE sp_dirtyread_tc1_T2_error @madh varchar(20)
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('khach_hang') = 0 AND IS_ROLEMEMBER('dbowner') = 0
