86cd3b0d2b076985bf6621e4a6e6036d879f0244
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-12-16 23:38:46 +0700

    fix bug deadlock  testcase1/T2_fixed

--- scripts/DEAD LOCK/Test_case_1_19127420/T2_fixed.sql
+++ scripts/DEAD LOCK/Test_case_1_19127420/T2_fixed.sql
@@ -4,7 +4,7 @@
 -- T2 - Tai Xe
 USE OnlineOrderingSystem
 GO
-CREATE PROCEDURE sp_deadlock_tc1_T2_fixed @madh varchar(20),@ttdh nvarchar(50)
+ALTER PROCEDURE sp_deadlock_tc1_T2_fixed @madh varchar(20),@ttdh nvarchar(50), @matx varchar(20)
 AS
 BEGIN TRAN 
 	IF IS_ROLEMEMBER('tai_xe') = 0 AND IS_ROLEMEMBER('dbowner') = 0
@@ -13,11 +13,18 @@ BEGIN TRAN
 		END
 	ELSE
 		BEGIN
-			SET TRAN ISOLATION LEVEL SERIALIZABLE
-			SELECT TenDuong FROM DON_HANG WITH (UPDLOCK) WHERE MaDH = @madh
-			UPDATE DON_HANG WITH (XLOCK)
-			SET TinhTrangDH = @ttdh
-			WHERE MaDH = @madh
-			COMMIT TRAN
+			IF EXISTS (SELECT* FROM GIAO_HANG WHERE MaDH = @madh AND MaTX = @matx)
+				BEGIN
+					ROLLBACK TRAN
+				END
+			ELSE
+				BEGIN
+					SELECT TenDuong FROM DON_HANG WITH (UPDLOCK) WHERE MaDH = @madh
+					UPDATE DON_HANG WITH (XLOCK)
+					SET TinhTrangDH = @ttdh
+					WHERE MaDH = @madh
+					INSERT INTO GIAO_HANG
+					VALUES (@matx,@madh,NULL)
+					COMMIT TRAN	
+				END
 		END
-
